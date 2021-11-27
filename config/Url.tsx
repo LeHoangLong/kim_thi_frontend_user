cc871bfde3a8fcd71f391a02bf5f1bc44a688aa3
Le Hoang Long <le.hoang.long@outlook.com>
2021-11-28 04:19:53 +0800

    Added backend url for release mode

--- config/Url.tsx
+++ config/Url.tsx
@@ -1,7 +1,22 @@
+let isRelease = false
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    // dev code
+} else {
+    // production code
+    isRelease = true
+}
+
 let _BACKEND_URL = "http://kim_thi_backend_1/backend/enduser"
-if (process.browser) {
+if (isRelease) {
+    _BACKEND_URL = "https://kim-thi-backend-rfqj7mlw2q-as.a.run.app/backend/enduser"
+} else if (process.browser) {
     _BACKEND_URL = "http://localhost/backend/enduser"
 }
 
 export const BACKEND_URL = _BACKEND_URL;
-export const FILESERVER_URL = "http://localhost/backend";
+
+let _FILESERVER_URL = "http://localhost/backend" 
+if (isRelease) {
+    _FILESERVER_URL = 'https://kim-thi-backend-rfqj7mlw2q-as.a.run.app/backend'
+}
+export const FILESERVER_URL = _FILESERVER_URL;
