a1ebe61031961c471479406794af961d79a8131d
Le Hoang Long <le.hoang.long@outlook.com>
2022-02-27 23:20:15 +0800

    Added icon

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,7 @@ class MyDocument extends Document {
                 `,
                 }}
             />
+            <link rel="icon" href="/public/logos/logo.png"></link>
             <meta name="viewport" content="width=device-width,initial-scale=1"/>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
