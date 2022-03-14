ac6c3e0238aa15351b5ad3a5445767a9b4148b5d
Le Hoang Long <le.hoang.long@outlook.com>
2022-03-14 21:56:31 +0800

    Added sitemap

---
+++ pages/sitemap.xml.ts
@@ -0,0 +1,85 @@
+import React from "react";
+import fs from 'fs';
+import path from 'path';
+import container from '../container'
+import { IProductRepositories } from "../repositories/IProductRepositories";
+import Symbols from "../config/Symbols";
+
+const Sitemap = () => {};
+
+function getFiles(dir) : string[] {
+    const subdirs = fs.readdirSync(dir);
+    let files : string[] = []
+    subdirs.forEach((subdir) => {
+        let joinedPath = path.join(dir, subdir)
+        fs.statSync(joinedPath).isDirectory() ? 
+            files = files.concat(getFiles(joinedPath)) : 
+            files.push(subdir);
+
+    });
+    let filtered = files.filter((f) => (
+            f.includes('ts') || 
+            f.includes('js')
+        ) && !f.includes('[') && !f.includes('.xml')
+    );
+    return filtered
+}
+
+export const getServerSideProps = async ({ res }) => {
+    const baseUrl = process.env.NEXT_URL;
+    
+    const staticPages = getFiles("pages")
+    .filter((staticPage) => {
+        return ![
+        "_app.js",
+        "_document.js",
+        "_error.js",
+        "sitemap.xml.ts",
+        ].includes(staticPage) && (staticPage.includes('ts') || staticPage.includes('js'));
+    })
+    .map((staticPagePath) => {
+        return `${baseUrl}/${staticPagePath}`;
+    });
+
+    let productRepositories = container.get<IProductRepositories>(Symbols.PRODUCT_REPOSITORY)
+    let productCount = await productRepositories.getNumberOfProducts({
+        categories: [],
+        productSearch: '',
+    })
+    let productSummaries = await productRepositories.getProductSummaries({
+        categories: [],
+        limit: productCount,
+        offset: 0,
+        productSearch: '',
+    })
+
+    let pages = productSummaries.reduce((a, p) => a.concat(`${baseUrl}/products/${p.id}`), staticPages)
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${pages
+        .map((url) => {
+            return `
+            <url>
+                <loc>${url}</loc>
+                <lastmod>${new Date().toISOString()}</lastmod>
+                <changefreq>monthly</changefreq>
+                <priority>1.0</priority>
+            </url>
+            `;
+        })
+        .join("")}
+    </urlset>
+    `;
+
+    res.setHeader("Content-Type", "text/xml");
+    res.write(sitemap);
+    res.end();
+  
+    return {
+      props: {},
+    };
+  };
+  
+
+export default Sitemap;
