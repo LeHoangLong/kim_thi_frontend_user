dc3f4baeb20aeb046ad5df0dcbed946cf3256f81
Le Hoang Long <le.hoang.long@outlook.com>
2022-03-15 20:52:59 +0800

    Fixed sitemap

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -35,10 +35,11 @@ export const getServerSideProps = async ({ res }) => {
         "_document.js",
         "_error.js",
         "sitemap.xml.ts",
+        "index.tsx"
         ].includes(staticPage) && (staticPage.includes('ts') || staticPage.includes('js'));
     })
     .map((staticPagePath) => {
-        return `${baseUrl}/${staticPagePath}`;
+        return `${baseUrl}/${staticPagePath.split('.')[0]}`;
     });
 
     let productRepositories = container.get<IProductRepositories>(Symbols.PRODUCT_REPOSITORY)
@@ -57,6 +58,12 @@ export const getServerSideProps = async ({ res }) => {
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>${baseUrl}</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <changefreq>monthly</changefreq>
+            <priority>1.0</priority>
+        </url>
         ${pages
         .map((url) => {
             return `
